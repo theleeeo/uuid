7b9c7fd7e0a20eb90e90e90b912f8c515bfc3e6f
Leonard Forsberg <leonard.forsberg@vx.se>
2023-09-21 09:51:03 +0200

    it was enough with just gray

--- go.mod
+++ go.mod
@@ -4,16 +4,10 @@ go 1.19
 
 require (
 	github.com/google/uuid v1.3.0
-	github.com/muesli/termenv v0.15.2
 	golang.design/x/clipboard v0.7.0
 )
 
 require (
-	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.18 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.6.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/muesli/termenv"
-
 	"github.com/google/uuid"
 	"golang.design/x/clipboard"
 )
 
 const (
 	colorReset = "\033[0m"
-	colorBlack = "\033[30m"
 	colorGray  = "\033[38;2;128;128;128m"
 )
 
@@ -32,27 +29,7 @@ func main() {
 }
 
 func getPrintFunction() func(string) {
-	r, g, b := getTerminalColor()
-	br := getBrightness(r, g, b)
-
-	if br < 3 {
-		return func(s string) {
-			fmt.Println(colorGray + s + colorReset)
-		}
-	} else {
-		return func(s string) {
-			fmt.Println(colorBlack + s + colorReset)
-		}
+	return func(s string) {
+		fmt.Println(colorGray + s + colorReset)
 	}
-}
-
-func getTerminalColor() (r int, g int, b int) {
-	seq := termenv.BackgroundColor().Sequence(true)
-
-	fmt.Sscanf(seq, "48;2;%d;%d;%dm", &r, &g, &b)
-	return
-}
-
-func getBrightness(r, g, b int) float64 {
-	return 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b)
 }
